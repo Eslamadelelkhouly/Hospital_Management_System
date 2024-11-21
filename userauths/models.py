38d54eadeef0282581e4abe1df8d9ec3b678e118
Eslamadelelkhouly <rodyerak@gmail.com>
2024-11-21 14:42:17 +0200

    add url sign-up

--- userauths/models.py
+++ userauths/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, Group, Permission
 
+USER_TYPE = (
+    ("Doctor" , "Doctor"),
+    ("Patient" , "Patient"),
+)
+
+
 class User(AbstractUser):
     email = models.EmailField(unique=True, max_length=500)
     username = models.CharField(max_length=100 , null=True, blank=True)
+    user_type = models.CharField(max_length=50 , choices=USER_TYPE , null=True , blank=True , default=None)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ['username']
--- userauths/views.py
+++ userauths/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render , redirect
 from django.contrib import messages
+from django.contrib.auth import authenticate , login , logout
 from userauths import forms as userauths_forms
 # Create your views here.
 
 def register_view(request):
-    if request.user.is_authenticated:
-        messages.success(request , "You are Already loggged in")
-        return redirect("/")
+    # if request.user.is_authenticated:
+    #     messages.success(request , "You are Already loggged in")
+    #     return redirect("/")
     
-    form = userauths_forms.UserRegisterForm()
+    form = userauths_forms.UserRegisterForm(request.POST or None)
+    if form.is_valid():
+        user = form.save()
+        full_name = form.cleaned_data.get("full_name")
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user_ = authenticate(email=email,password=password)
+        login(request,user_)
+        messages.success(request,"Account created successfully")
+        return redirect("/")
     
     context = {
         "form":form
