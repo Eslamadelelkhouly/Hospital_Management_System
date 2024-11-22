9577666d6d1ccce6177468db1082a46b1e93a8a6
Eslamadelelkhouly <rodyerak@gmail.com>
2024-11-22 20:07:20 +0200

    add templates

--- userauths/forms.py
+++ userauths/forms.py
@@ -5,15 +5,21 @@ from userauths.models import User     # import models from app userauths
 
 # widget = froms.TextInput() -> input in templates
 
+USER_TYPE = [
+    ("Doctor" , "Doctor"),
+    ("Patient" , "Patient"),
+]
+
 class UserRegisterForm(UserCreationForm):
-    full_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control' , 'placeholder':'Name'}))
-    email = forms.EmailField(widget=forms.TextInput(attrs={'class':'form-control' , 'placeholder':'Email'}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control' , 'placeholder':'Password'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control' , 'placeholder':'Again password'}))
+    full_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control' , 'placeholder':'Eslam adel'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class':'form-control' , 'placeholder':'eslam@gmail.com'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control' , 'placeholder':'***********'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control' , 'placeholder':'***********'}))
+    user_type = forms.ChoiceField(choices=USER_TYPE , widget=forms.Select(attrs={'class':'form-select' }))
 
     class Meta:
         model = User
-        fields = ['full_name','email','password1','password2']
+        fields = ['full_name','email','password1','password2', 'user_type']
 
 
 class LoginForm(forms.Form):
--- userauths/urls.py
+++ userauths/urls.py
@@ -4,5 +4,8 @@ from userauths import views
 app_name = "userauths"
 
 urlpatterns = [
-    path("sign-up/",views.register_view, name="sign-up")
+    path("sign-up/",views.register_view, name="sign-up"),
+    path("sign-in/",views.login_view, name="sign-in"),
+    path("sign-out/",views.logout, name="sign-out"),
+
 ]
