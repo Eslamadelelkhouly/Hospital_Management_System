ec6744394cf8f4ffe85e59ead57320c32079be99
Eslamadelelkhouly <rodyerak@gmail.com>
2024-11-24 16:44:57 +0200

    add splash & description services  page

--- base/views.py
+++ base/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-
+from base import models as base_models
 # Create your views here.
 def index(request):
-    return render(request , "base/index.html")
+    service = base_models.Service.objects.all()
+    context = {
+        "services":service
+    }
+    return render(request , "base/index.html" , context=context)
 
 def service_detail(request , service_id):
-    return render(request , "base/service_detail.html")
+    services = base_models.Service.objects.get(id=service_id)
+    context = {
+        "services" : services
+    }
+    return render(request , "base/service_detail.html",context=context)
